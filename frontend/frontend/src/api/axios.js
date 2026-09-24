136b9632b40640f2788067d4f926d9f5d9d961e4
Chitoiu Robert <chitoiurobert123@gmail.com>
2026-09-24 10:20:03 +0300

    add respone interceptor for handling 401 error

--- frontend/frontend/src/api/axios.js
+++ frontend/frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ACCESS_TOKEN } from '../constants'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
@@ -14,5 +14,17 @@ api.interceptors.request.use((config) => {
 }, (error) => {
     return Promise.reject(error)
 })
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem(ACCESS_TOKEN)
+            localStorage.removeItem(REFRESH_TOKEN)
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default api
